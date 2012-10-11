7afb15b6727c02cb88999572614ea0f00ece78fb
Bediko <bediko@chaosdorf.de>
2012-10-11 11:27:02 +0200

    Add Server and Client class.

---
+++ server/server/Server.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author bediko
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
