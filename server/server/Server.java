17a524a8b22317c860758a125a095b132735c206
Bediko <bediko@chaosdorf.de>
2012-10-13 13:40:55 +0200

    Beautify code

--- server/server/Server.java
+++ server/server/Server.java
@@ -9,13 +9,10 @@ import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-
-
-
 public class Server extends UnicastRemoteObject implements Client_conn {
 
 	public Server() throws RemoteException {
-		super();	// TODO Auto-generated constructor stub
+		super(); // TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -23,32 +20,32 @@ public class Server extends UnicastRemoteObject implements Client_conn {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setSecurityManager (new RMISecurityManager ());
+		System.setSecurityManager(new RMISecurityManager());
 		Server server = null;
 		try {
-			server = new Server ();
+			server = new Server();
 		} catch (RemoteException e) {
 			e.printStackTrace();
 			System.exit(0);
 		}
 		try {
-			Naming.bind ("rmi://127.0.0.1:9090/server",server);
-		}catch(AlreadyBoundException ex){
+			Naming.bind("rmi://127.0.0.1:9090/server", server);
+		} catch (AlreadyBoundException ex) {
 			try {
-				Naming.rebind ("rmi://127.0.0.1:9090/server",server);
+				Naming.rebind("rmi://127.0.0.1:9090/server", server);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} 
-		}
-		catch (Exception ex) {
-			System.out.println(ex.getMessage ());
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
 		}
 
 	}
+
 	@Override
 	public void getMessage(String message) throws RemoteException {
-		System.out.println(message);	
+		System.out.println(message);
 	}
 
 }
