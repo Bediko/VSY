5b928fe9dd2cd1b734272d6b9826cc0f811bdb11
Bediko <bediko@chaosdorf.de>
2012-10-13 13:36:29 +0200

    Add Client class, add Client_conn Interface, add Server class, add function to print messages from client on the server, fix bug where the server could not reconnect to the rmiregistry after restart.

--- client/client/Client.java
+++ client/client/Client.java
@@ -3,6 +3,12 @@
  */
 package client;
 
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+
+import server.Client_conn;
+
+
 
 public class Client {
 
@@ -10,7 +16,14 @@ public class Client {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setSecurityManager (new RMISecurityManager ());
+				try {
+					Client_conn conn = (Client_conn)
+					Naming.lookup("rmi://127.0.0.1:9090/server");
+					conn.getMessage(args[0]);
+				} catch ( Exception ex ) {
+					System.out.println ("Exception:"+ ex.getMessage ());
+				}
 
 	}
 
--- server/server/Client_conn.java
+++ server/server/Client_conn.java
@@ -1,7 +1,9 @@
 package server;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Client_conn extends Remote {
-
+	void getMessage(String message)
+		throws RemoteException;
 }
